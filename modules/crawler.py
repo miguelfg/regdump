98a9bb63bb1e9a9b1d3bd9a817ba2e5e23b8df37
Miguel Fiandor <miguel.fiandor.gutierrez@gmail.com>
2016-03-09 15:41:35 +0100

    styles

--- modules/crawler.py
+++ modules/crawler.py
@@ -1,19 +1,19 @@
 import requests
-from Classes import Sociedad,Persona,Asociacion,Fundacion,FundacionPersonas
+from Classes import Sociedad, Persona, Asociacion, Fundacion, FundacionPersonas
 from queue import Empty
 import logging
 import Classes
 from Classes import *
 
-from modules import parser,db_worker
-from bs4 import BeautifulSoup,SoupStrainer
+from modules import parser, db_worker
+from bs4 import BeautifulSoup, SoupStrainer
 from queue import Queue
-from Query import Query,SociedadQuery,FichaQuery
+from Query import Query, SociedadQuery, FichaQuery
 from Worker import ProcessHTML
 import Worker
 from time import sleep
 from threading import active_count
-from urllib3 import HTTPConnectionPool,ProxyManager
+from urllib3 import HTTPConnectionPool, ProxyManager
 import asyncio
 from urllib3 import make_headers
 import aiohttp
@@ -22,118 +22,140 @@ from random import sample
 logger = logging.getLogger('crawler')
 sem = asyncio.Semaphore(1)
 lock = asyncio.Lock()
-user_agents = ['Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0', 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36' , 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25', 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)' , 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)', 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2']
+user_agents = ['Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0',
+               'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36',
+               'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25',
+               'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+               'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+               'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25',
+               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2']
 old_fichas = db_worker.get_fichas()
 
-def query_url(page,query):
-    return ('http://201.224.39.199/scripts/nwwisapi.dll/conweb/MESAMENU?TODO=MER4&START=%s&FROM=%s' % (str(page),query))
+
+def query_url(page, query):
+    return (
+        'http://201.224.39.199/scripts/nwwisapi.dll/conweb/MESAMENU?TODO=MER4&START=%s&FROM=%s' % (str(page), query))
+
 
 def ficha_url(ficha):
     return ('http://201.224.39.199/scripts/nwwisapi.dll/conweb/MESAMENU?TODO=SHOW&ID=%s' % str(ficha))
 
-def ficha_generator(fichas,old_fichas):
+
+def ficha_generator(fichas, old_fichas):
     for i in fichas:
         if i not in old_fichas:
             yield ficha_url(i)
         else:
             continue
 
-def brute_fundaciones(start,stop,step):
-    queue=[]
-    fichas = range(start,stop,step)
+
+def brute_fundaciones(start, stop, step):
+    queue = []
+    fichas = range(start, stop, step)
     lock = asyncio.Lock()
     loop = asyncio.get_event_loop()
-    f = asyncio.wait([get_html(fc,queue,parse_fundacion_html) for fc in fichas])
+    f = asyncio.wait([get_html(fc, queue, parse_fundacion_html) for fc in fichas])
     loop.run_until_complete(f)
     return queue
 
-def brute_sociedades(start,stop,step):
-    fichas = range(start,stop,step)
-    queue=[]
+
+def brute_sociedades(start, stop, step):
+    fichas = range(start, stop, step)
+    queue = []
     lock = asyncio.Lock()
     loop = asyncio.get_event_loop()
-    f = asyncio.wait([get_html(fc,queue,parse_sociedad_html) for fc in fichas if fc not in old_fichas])
+    f = asyncio.wait([get_html(fc, queue, parse_sociedad_html) for fc in fichas if fc not in old_fichas])
     loop.run_until_complete(f)
     return queue
+
 
 def generate_urls(url):
     page = 0
     while True:
-        yield ( query_url(i,url)for i in range(page*15,(page+1)*15,15) )
+        yield (query_url(i, url) for i in range(page * 15, (page + 1) * 15, 15))
         page += 1
+
 
 @asyncio.coroutine
 def get(*args, **kwargs):
     response = yield from aiohttp.request('GET', *args, **kwargs)
     return (yield from response.read())
 
+
 @asyncio.coroutine
-def get_html(url,queue,parser):
-    headers=make_headers(user_agent=sample(user_agents,1)[0])
-    #conn = aiohttp.ProxyConnector(proxy='http://localhost:8118')
+def get_html(url, queue, parser):
+    headers = make_headers(user_agent=sample(user_agents, 1)[0])
+    # conn = aiohttp.ProxyConnector(proxy='http://localhost:8118')
     with (yield from sem):
         if parser != parse_query_result:
-            body = yield from get(ficha_url(url), headers=headers) #, connector=conn)
+            body = yield from get(ficha_url(url), headers=headers)  # , connector=conn)
         elif parser == parse_query_result:
-            body = yield from get(url, headers=headers) #, connector=conn)
+            body = yield from get(url, headers=headers)  # , connector=conn)
         sleep(4)
     with (yield from lock):
         [queue.append(i) for i in parser(body)]
 
+
 def query_registro_publico(query):
-  old = set()
-  urls = generate_urls(query)
-  loop = asyncio.get_event_loop()
-  results = set()
-  while True:
-    queue = []
-    f = asyncio.wait([get_html(url,queue,parse_query_result) for url in next(urls)])
-    loop.run_until_complete(f)
-    results.update(queue)
-    if results == old:
-        return set(filter(None,results))
-    else:
-        old = results
+    old = set()
+    urls = generate_urls(query)
+    loop = asyncio.get_event_loop()
+    results = set()
+    while True:
+        queue = []
+        f = asyncio.wait([get_html(url, queue, parse_query_result) for url in next(urls)])
+        loop.run_until_complete(f)
+        results.update(queue)
+        if results == old:
+            return set(filter(None, results))
+        else:
+            old = results
+
 
 def query(query):
     fichas = query_registro_publico(query)
-    return brute_sociedades(fichas,True)
+    return brute_sociedades(fichas, True)
+
 
 def parse_query_result(html):
-    soup = BeautifulSoup(html,"lxml", parse_only=SoupStrainer('table'))
-    sociedades = soup.find('th',text='NOMBRE SOCIEDAD').parent.next_siblings
+    soup = BeautifulSoup(html, "lxml", parse_only=SoupStrainer('table'))
+    sociedades = soup.find('th', text='NOMBRE SOCIEDAD').parent.next_siblings
     fichas = [int(row.find('a').string) for row in sociedades if row.td.string is not None]
     return fichas
 
+
 def parse_fundacion_html(html):
-    html = html.decode('ISO-8859-1','ignore')
+    html = html.decode('ISO-8859-1', 'ignore')
     if parser.exists(html):
         soup = BeautifulSoup(html, 'html.parser', parse_only=SoupStrainer('p'))
         fundacion = scrape_fundacion_data(soup)
         logger.debug('found fundacion %s', fundacion.nombre)
-        personas,asociaciones = scrape_fundacion_personas(soup)
+        personas, asociaciones = scrape_fundacion_personas(soup)
         logger.debug('found %i personas', len(personas))
         logger.debug('found %i asociaciones', len(asociaciones))
-        return db_worker.resolve_fundacion(fundacion,personas,asociaciones)
+        return db_worker.resolve_fundacion(fundacion, personas, asociaciones)
+
 
 def parse_sociedad_html(html):
-    html = html.decode('ISO-8859-1','ignore')
+    html = html.decode('ISO-8859-1', 'ignore')
     if parser.exists(html):
         soup = BeautifulSoup(html, 'html.parser', parse_only=SoupStrainer('p'))
         sociedad = scrape_sociedad_data(soup)
         logger.info('found sociedad %s', sociedad.nombre)
-        personas,asociaciones = scrape_personas(soup)
+        personas, asociaciones = scrape_personas(soup)
         logger.debug('found %i personas', len(personas))
         logger.debug('found %i asociaciones', len(asociaciones))
-        return db_worker.resolve_sociedad(sociedad,personas,asociaciones)
+        return db_worker.resolve_sociedad(sociedad, personas, asociaciones)
+
 
 def scrape_fundacion_personas(soup):
     miembros = scrape_fundacion_miembros(soup)
     fundadores = scrape_fundacion_fundadores(soup)
     cargos = scrape_fundacion_cargos(soup)
     personas = set().union(*[miembros, unpack_personas_in_dignatarios(cargos), fundadores])
-    asociaciones = dict(list({'miembros':miembros, 'fundadores':fundadores}.items()) + list(cargos.items()))
-    return [personas,asociaciones]
+    asociaciones = dict(list({'miembros': miembros, 'fundadores': fundadores}.items()) + list(cargos.items()))
+    return [personas, asociaciones]
+
 
 def scrape_fundacion_miembros(soup):
     try:
@@ -143,6 +165,7 @@ def scrape_fundacion_miembros(soup):
         return set()
     return miembros
 
+
 def scrape_fundacion_fundadores(soup):
     try:
         fundadores = {Persona(persona) for persona in parser.collect_fundadores(soup)}
@@ -151,21 +174,25 @@ def scrape_fundacion_fundadores(soup):
         return {}
     return fundadores
 
+
 def scrape_fundacion_cargos(soup):
-  try:
-      cargos = { item[0]: {Persona(value) for value in item[1]} for item in parser.collect_cargos(soup).items()}
-  except Exception as e:
-    print(e)
-    return {}
-  return cargos
+    try:
+        cargos = {item[0]: {Persona(value) for value in item[1]} for item in parser.collect_cargos(soup).items()}
+    except Exception as e:
+        print(e)
+        return {}
+    return cargos
+
 
 def scrape_personas(soup):
     directores = scrape_sociedad_directores(soup)
     subscriptores = scrape_sociedad_subscriptores(soup)
     dignatarios = scrape_sociedad_dignatarios(soup)
     personas = set().union(*[directores, unpack_personas_in_dignatarios(dignatarios), subscriptores])
-    asociaciones = dict(list({'directores':directores, 'subscriptores':subscriptores}.items()) + list(dignatarios.items()))
-    return [personas,asociaciones]
+    asociaciones = dict(
+        list({'directores': directores, 'subscriptores': subscriptores}.items()) + list(dignatarios.items()))
+    return [personas, asociaciones]
+
 
 def scrape_sociedad_directores(soup):
     try:
@@ -175,6 +202,7 @@ def scrape_sociedad_directores(soup):
         return set()
     return directores
 
+
 def scrape_sociedad_subscriptores(soup):
     try:
         subscriptores = {Persona(persona) for persona in parser.collect_subscriptores(soup)}
@@ -183,19 +211,23 @@ def scrape_sociedad_subscriptores(soup):
         return {}
     return subscriptores
 
+
 def scrape_sociedad_dignatarios(soup):
-  try:
-      dignatarios = { item[0]: {Persona(value) for value in item[1]} for item in parser.collect_dignatarios(soup).items()}
-  except Exception as e:
-    print(e)
-    return {}
-  return dignatarios
+    try:
+        dignatarios = {item[0]: {Persona(value) for value in item[1]} for item in
+                       parser.collect_dignatarios(soup).items()}
+    except Exception as e:
+        print(e)
+        return {}
+    return dignatarios
+
 
 def unpack_personas_in_dignatarios(dignatarios):
     return {val for sublist in dignatarios.values() for val in sublist}
 
+
 def scrape_sociedad_data(soup):
-    sociedad = Sociedad(parser.collect_nombre_sociedad(soup),parser.collect_ficha(soup))
+    sociedad = Sociedad(parser.collect_nombre_sociedad(soup), parser.collect_ficha(soup))
     sociedad.fecha_registro = parser.collect_fecha_registro(soup)
     sociedad.provincia = parser.collect_provincia(soup)
     sociedad.notaria = parser.collect_notaria(soup)
@@ -208,8 +240,9 @@ def scrape_sociedad_data(soup):
     sociedad.representante_text = parser.collect_representante_text(soup)
     return sociedad
 
+
 def scrape_fundacion_data(soup):
-    fundacion = Fundacion(parser.collect_nombre_fundacion(soup),parser.collect_ficha(soup))
+    fundacion = Fundacion(parser.collect_nombre_fundacion(soup), parser.collect_ficha(soup))
     fundacion.escritura = parser.collect_escritura(soup)
     fundacion.documento = parser.collect_documento(soup)
     fundacion.fecha_registro = parser.collect_fecha_registro(soup)
@@ -223,4 +256,3 @@ def scrape_fundacion_data(soup):
     fundacion.patrimonio_text = parser.collect_patrimonio_text(soup)
     fundacion.firmante_text = parser.collect_firmante_text(soup)
     return fundacion
-
