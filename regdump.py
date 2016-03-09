6d1f142b3de4dd1096d092fd7f70c0078180121b
Miguel Fiandor <miguel.fiandor.gutierrez@gmail.com>
2016-03-09 18:41:43 +0100

    args size

--- regdump.py
+++ regdump.py
@@ -19,8 +19,9 @@ logger = logging.getLogger('regdump')
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Query registro-publico.gob.pa for sociedades.')
     parser.add_argument('--query', dest='query', type=str)
-    parser.add_argument('--start', dest='start', type=int, default=0)
-    parser.add_argument('--stop', dest='stop', type=int, default=10)
+    parser.add_argument('--start', dest='start', type=int, default=None)
+    parser.add_argument('--stop', dest='stop', type=int, default=None)
+    parser.add_argument('--size', dest='size', type=int, default=100)
     parser.add_argument('--step', dest='step', type=int, default=1)
     args = parser.parse_args()
 
@@ -30,6 +31,13 @@ if __name__ == "__main__":
         logger.info('performing query: %s', str(args.query))
         sociedades = crawler.query(args.query)
     else:
+        if not args.start:
+            # get last index of record in database
+            sociedades_ids = crawler.old_fichas
+            max_id = max(sociedades_ids)
+            args.start = max_id
+        if not args.stop:
+            args.stop = args.start + args.size
         sociedades = crawler.brute_sociedades(args.start, args.stop, args.step)
 
     logger.info('found %i sociedades', len(sociedades))
