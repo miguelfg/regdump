5a60a4f4a53792cf8b6cc98e8d64a079394730bd
Miguel Fiandor <miguel.fiandor.gutierrez@gmail.com>
2016-04-05 13:43:39 +0200

    query remove + logging fix

--- regdump.py
+++ regdump.py
@@ -26,28 +26,20 @@ if __name__ == "__main__":
 
     logger.info('regdump started')
 
-    if args.query:
-        logger.info('performing query: %s', str(args.query))
-        sociedades = crawler.query(args.query)
-    else:
-        if args.start is None:
-            # get last index of record in database
-            # sociedades_ids = crawler.old_fichas
-            # print(len(sociedades_ids))
-            # print(max(sociedades_ids))
-            # max_id = max(sociedades_ids)
-            # args.start = max_id
-            args.start = db_worker.find_max_ficha()
-            logger.info('found %i sociedades already in DB', args.start)
+    if args.start is None:
+        # get last index of record in database
+        # sociedades_ids = crawler.old_fichas
+        # print(len(sociedades_ids))
+        # print(max(sociedades_ids))
+        # max_id = max(sociedades_ids)
+        # args.start = max_id
+        args.start = db_worker.find_max_ficha()
+        logger.info('found %i sociedades already in DB', args.start)
 
-        if not args.stop:
-            args.stop = args.start + args.size
+    if not args.stop:
+        args.stop = args.start + args.size
 
         logger.info('it will scrape from %i to %i', args.start, args.stop)
         sociedades = crawler.brute_sociedades(args.start, args.stop, args.step)
 
-
-    logger.info('found %i sociedades', len(sociedades))
-    logger.info('found %i personas',
-                len([item for sublist in [sociedad.personas for sociedad in sociedades] for item in sublist]))
     logger.info('regdump finished')
